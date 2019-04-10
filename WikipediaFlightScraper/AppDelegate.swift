c36b1ff4dae3cf2b0c41ee4f81307ae1a5c32070
Jacob Hausmann <jacob@wireless-10-146-1-108.public.utexas.edu>
2019-04-10 10:36:43 -0500

    Initial commit

---
+++ WikipediaFlightScraper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WikipediaFlightScraper
+//
+//  Created by Jacob Hausmann on 4/10/19.
+//  Copyright © 2019 Jacob Hausmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
